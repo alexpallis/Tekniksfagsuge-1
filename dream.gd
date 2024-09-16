88b68899576c503db2c2da37eab7fdd51c5b4048
Crafteren <oscarwegmann@live.dk>
2024-09-16 20:33:39 +0200

    enermy

---
+++ dream.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+const SPEED = 50
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _on_body_entered(body):
+	pass
+	
+	
+func _physics_process(delta):
+	
+	var direction = Input.get_vector("move left","move right","move up","move down")
+	
+	velocity = direction * SPEED
+	
+	if direction.x < 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk left right")
+	
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk left right")
+	
+	if direction.y < 0:
+		animated_sprite.play("walk up")
+	
+	if direction.y > 0:
+		animated_sprite.play("walk down")
+	
+	if Input.is_action_just_released("move left") or Input.is_action_just_released("move right"):
+		animated_sprite.play("idl left right")
+	
+	if Input.is_action_just_released("move down"):
+		animated_sprite.play("idl down")
+	
+	if Input.is_action_just_released("move up"):
+		animated_sprite.play("idl up")
+	
+	move_and_slide()
