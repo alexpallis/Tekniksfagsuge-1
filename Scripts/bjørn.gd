390f18f5521f6a2096cd94facc1f41065b19f105
Crafteren <oscarwegmann@live.dk>
2024-09-17 18:23:59 +0200

    eanermy movement

---
+++ Scripts/bjørn.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const  SPEED = 175
+var player_chase = false
+var player = null
+
+func _physics_process(delta):
+	if player_chase:
+		position += (player.position - position)/SPEED
+
+func _on_detection_body_entered(body):
+	player = body
+	player_chase = true
+
+func _on_detection_body_exited(body):
+	player = null
+	player_chase = false
