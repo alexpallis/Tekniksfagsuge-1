1a7858042a0d2c16bcfa73374426cc0cb87de7a1
Crafteren <oscarwegmann@live.dk>
2024-09-19 21:14:03 +0200

    papirs fly timer

--- Scripts/papirsfly.gd
+++ Scripts/papirsfly.gd
@@ -4,13 +4,19 @@ const  SPEED = 0.50
 var player_chase = false
 var player = null
 const knock_back_SPEED = 150
+var life_timer = true
 
 var health = 1
 var player_inattack_zone = false
 var can_take_damage = true
+
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var life_timer_timer = $"Life Timer Timer"
 
-
+func _ready():
+	if life_timer == true:
+		life_timer_timer.start()
+		life_timer = false
 
 func _physics_process(delta):
 	deal_with_damage()
@@ -85,3 +91,8 @@ func _on_papirsly_hitbox_body_exited(body):
 func _on_dead_area_body_entered(body):
 	if body.has_method("taget"):
 		self.queue_free()
+
+
+func _on_life_timer_timer_timeout():
+	self.queue_free()
+	print("hi %d" % [Time.get_time_dict_from_system().second])
