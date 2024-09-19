1368765924782ab46f62eb0538d2b049b9a36432
114234-Kiwi <krwi1@elev.tec.dk>
2024-09-19 21:24:40 +0200

    pause menu

--- Scens/pause_menu.gd
+++ Scens/pause_menu.gd
@@ -14,11 +14,9 @@ func _on_enemy_spawner_toggle_game_paused(is_paused : bool):
 		hide()
 
 
+func _on_quitbutton_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scens/main_menu.tscn")
+
 
 func _on_resumebutton_pressed() -> void:
 	enemy_spawner.game_paused = false
-
-
-
-func _on_quitbutton_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scens/main_menu.tscn")
