db45deab241618afd06a398e773f813f62729b46
Crafteren <oscarwegmann@live.dk>
2024-09-19 21:57:40 +0200

    gumball

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -2,12 +2,12 @@ extends Area2D
 
 @onready var coin_noise = $"Coin noise"
 @onready var game_manager = $GameManager
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-
-
-
+func _physics_process(delta):
+	animated_sprite_2d.play("Dreamcat")
+	
 func _on_body_entered(body):
 	if body.has_method("player"):
-		coin_noise.play()
 		self.queue_free()
-		
+		coin_noise.play()
