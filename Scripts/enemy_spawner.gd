f0a5ee7cadf68a18e99bbac1f4d686cb7b5b3ddb
114234-Kiwi <krwi1@elev.tec.dk>
2024-09-19 23:42:21 +0200

    dream stuff

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -108,3 +108,12 @@ var game_paused : bool = false:
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("ui_cancel")):
 		game_paused = !game_paused
+
+#score stuff
+
+var score = 0
+
+func add_dream_point():
+	score += 1
+	print(score)
+	
