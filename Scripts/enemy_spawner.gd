1e1971bd447791bc5b4b3f6f2f17169b74b57dbd
114234-Kiwi <krwi1@elev.tec.dk>
2024-09-20 13:36:56 +0200

    pause

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -153,21 +153,7 @@ func _process(delta):
 		Generator.level = 5
 		level_2.start()
 
-#pause menu
 
-signal toggel_game_paused(is_paused : bool)
-
-var game_paused : bool = false:
-	get:
-		return game_paused
-	set(value):
-		game_paused = value
-		get_tree().paused = game_paused
-		emit_signal("toggel_game_paused", game_paused)
-
-func _input(event: InputEvent) -> void:
-	if(event.is_action_pressed("ui_cancel")):
-		game_paused = !game_paused
 
 #score stuff
 
